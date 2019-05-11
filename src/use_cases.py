c5d73f0890740af86eab47c26cc64ea4d553b404
Garrett Klatte <gklatte@ucla.edu>
2019-05-11 14:48:29 +0000

    Implement difficulty collection endpoints

--- src/use_cases.py
+++ src/use_cases.py
@@ -1,4 +1,6 @@
 """Use cases."""
+from typing import List
+
 from definitions.context import Context
 from definitions.graph import Graph
 
@@ -6,3 +8,8 @@ from definitions.graph import Graph
 def retrieve_graph(context: Context, difficulty: str, uri: int) -> Graph:
     """Using 'context', retrieve the graph uniquely identified by 'difficulty' and 'uri'."""
     return context.graph_gateway.fetch_graph(difficulty, uri)
+
+
+def retrieve_graphs(context: Context, difficulty: str) -> List[int]:
+    """Using 'context', retrieve a list of uris for graphs with 'difficulty'."""
+    return context.graph_gateway.fetch_uris(difficulty)
